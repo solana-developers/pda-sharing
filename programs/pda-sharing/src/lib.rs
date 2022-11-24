58fc8759893949da302229f68fa3e86200156c65
john <johnzhengyuliu@gmail.com>
2022-11-24 09:08:21 -0600

    starter

--- programs/pda-sharing/src/lib.rs
+++ programs/pda-sharing/src/lib.rs
@@ -21,23 +21,6 @@ pub mod pda_sharing {
         let seeds = &[ctx.accounts.pool.mint.as_ref(), &[ctx.accounts.pool.bump]];
         token::transfer(ctx.accounts.transfer_ctx().with_signer(&[seeds]), amount)
     }
-
-    pub fn initialize_pool_recommended(ctx: Context<InitializePoolRecommended>) -> Result<()> {
-        ctx.accounts.pool.vault = ctx.accounts.vault.key();
-        ctx.accounts.pool.mint = ctx.accounts.mint.key();
-        ctx.accounts.pool.withdraw_destination = ctx.accounts.withdraw_destination.key();
-        ctx.accounts.pool.bump = *ctx.bumps.get("pool").unwrap();
-        Ok(())
-    }
-
-    pub fn withdraw_recommended(ctx: Context<WithdrawTokensRecommended>) -> Result<()> {
-        let amount = ctx.accounts.vault.amount;
-        let seeds = &[
-            ctx.accounts.pool.withdraw_destination.as_ref(),
-            &[ctx.accounts.pool.bump],
-        ];
-        token::transfer(ctx.accounts.transfer_ctx().with_signer(&[seeds]), amount)
-    }
 }
 
 #[derive(Accounts)]
@@ -54,32 +37,6 @@ pub struct InitializePool<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
     pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct InitializePoolRecommended<'info> {
-    #[account(
-        init,
-        payer = payer,
-        space = 8 + 32 + 32 + 32 + 1,
-        seeds = [withdraw_destination.key().as_ref()],
-        bump
-    )]
-    pub pool: Account<'info, TokenPool>,
-    pub mint: Account<'info, Mint>,
-    #[account(
-        init,
-        payer = payer,
-        token::mint = mint,
-        token::authority = pool,
-    )]
-    pub vault: Account<'info, TokenAccount>,
-    pub withdraw_destination: Account<'info, TokenAccount>,
-    #[account(mut)]
-    pub payer: Signer<'info>,
-    pub system_program: Program<'info, System>,
-    pub token_program: Program<'info, Token>,
-    pub rent: Sysvar<'info, Rent>,
 }
 
 #[derive(Accounts)]
@@ -102,34 +59,6 @@ impl<'info> WithdrawTokens<'info> {
             from: self.vault.to_account_info(),
             to: self.withdraw_destination.to_account_info(),
             authority: self.authority.to_account_info(),
-        };
-        CpiContext::new(program, accounts)
-    }
-}
-
-#[derive(Accounts)]
-pub struct WithdrawTokensRecommended<'info> {
-    #[account(
-        has_one = vault,
-        has_one = withdraw_destination,
-        seeds = [withdraw_destination.key().as_ref()],
-        bump = pool.bump,
-    )]
-    pool: Account<'info, TokenPool>,
-    #[account(mut)]
-    vault: Account<'info, TokenAccount>,
-    #[account(mut)]
-    withdraw_destination: Account<'info, TokenAccount>,
-    token_program: Program<'info, Token>,
-}
-
-impl<'info> WithdrawTokensRecommended<'info> {
-    pub fn transfer_ctx(&self) -> CpiContext<'_, '_, '_, 'info, token::Transfer<'info>> {
-        let program = self.token_program.to_account_info();
-        let accounts = token::Transfer {
-            from: self.vault.to_account_info(),
-            to: self.withdraw_destination.to_account_info(),
-            authority: self.pool.to_account_info(),
         };
         CpiContext::new(program, accounts)
     }
