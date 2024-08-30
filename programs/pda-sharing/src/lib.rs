7d5e95c175d43f5c9aadd01afd2e6ed65e964f6c
Jeff Wood <wujunadmn@gmail.com>
2024-08-30 13:52:05 +0000

    correct programId

--- programs/pda-sharing/src/lib.rs
+++ programs/pda-sharing/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Mint, Token, TokenAccount};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("AdM5peDom1iMyEmzoR96sHa2eA8i5YvrZxkFU6HnfBzw");
 
 const DISCRIMINATOR_SIZE: usize = 8;
 
